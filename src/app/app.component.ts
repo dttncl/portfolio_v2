16d8e6228bb868a22435c823e36bec964b1a489b
dttncl <82695034+dttncl@users.noreply.github.com>
2024-02-16 18:26:50 -0500

    updated repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'portfolio-v2';
+  title = 'portfolio_v2';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then((m) => m.HomeComponent),
+      },
+    {
+        path: '',
+        loadComponent: () =>
+          import('./welcome/welcome.component').then((m) => m.WelcomeComponent),
+      },
+];
