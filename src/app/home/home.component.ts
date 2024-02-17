01c6717a87d04cf04da664c609875a62dea779ca
dttncl <82695034+dttncl@users.noreply.github.com>
2024-02-16 23:03:07 -0500

    added footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,19 @@ import { RouterModule } from '@angular/router';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ToolboxComponent } from '../toolbox/toolbox.component';
+import { FooterComponent } from '../footer/footer.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,NavigationComponent,ProjectsComponent,ToolboxComponent],
+  imports: [
+    RouterModule,
+    NavigationComponent,
+    ProjectsComponent,
+    ToolboxComponent,
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
