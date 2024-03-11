56036ed37ea87bae1a468f046eb927247104f1ba
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-11 18:33:31 -0400

    updated ken theme

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -26,15 +26,14 @@ export class NavigationComponent {
     this.viewportScroller.scrollToAnchor('toolbox-component');
   }
 
-  // bg, text
-  barbieTheme : string[] = ['#FFE7F8','#FF006E']
-  kenTheme : string[] = ['#CDEFFF','#3A8EE4']
+  // bg, h1, h2
+  barbieTheme : string[] = ['#FFE7F8','#FF006E','#592D3D']
+  kenTheme : string[] = ['#CDEFFF','#3A8EE4', '#16497d']
 
   isKen : boolean = false;
 
   switchTheme() {
-    //const links: HTMLCollectionOf<HTMLAnchorElement> = this.elRef.nativeElement.getElementsByTagName('a')
-    
+    // const links: HTMLCollectionOf<HTMLAnchorElement> = this.elRef.nativeElement.getElementsByTagName('a')
     if (this.isKen === false) {
 
       // switch to Ken Mode
@@ -42,28 +41,79 @@ export class NavigationComponent {
       this.isKen = true
 
       // toggle link colors
-      /*
-      for (let i = 0; i < links.length; i++) {
-        links[i].classList.remove('barbie');
-        links[i].classList.add('ken');
-      }
-      */
+      let links = Array.from(document.getElementsByClassName('nav-item'))
+      links.forEach(link => {
+        link.classList.remove('barbie');
+        link.classList.add('ken');
+      });
 
+      // toggle h2 colors
+      let h2s = Array.from(document.getElementsByTagName('h2'))
+      h2s.forEach(h2 => {
+        h2.style.color = this.kenTheme[2]
+      });
+
+      // toggle card colors
+      let cards = Array.from(document.getElementsByClassName('card'))
+      cards.forEach(card => {
+        card.classList.add('ken')
+      });
+
+      // toggle tag colors
+      let tags = Array.from(document.getElementsByClassName('tag'))
+      tags.forEach(tag => {
+        let onetag = tag as HTMLElement
+        onetag.style.color = this.kenTheme[1]
+        onetag.style.borderColor = this.kenTheme[1]
+      })
+    
       // toggle bg color
+      document.getElementsByTagName('main')[0].style.backgroundColor = this.kenTheme[0]
       document.getElementsByTagName('body')[0].style.backgroundColor = this.kenTheme[0]
+
+      // toggle h1, button
+      document.getElementsByTagName('h1')[0].style.color = this.kenTheme[1]
+      document.getElementById('open-cv')?.classList.add('ken')
 
     } else {
       // switch to Barbie Mode
       console.log("Hellooooo Barbie")
       this.isKen = false
-      /*
-      for (let i = 0; i < links.length; i++) {
-        links[i].classList.remove('ken');
-        links[i].classList.add('barbie');
-      }
-      */
+
+      // toggle link colors
+      let links = Array.from(document.getElementsByClassName('nav-item'))
+      links.forEach(link => {
+        link.classList.remove('ken');
+        link.classList.add('barbie');
+      });
+
+      // toggle h2 colors
+      let h2s = Array.from(document.getElementsByTagName('h2'))
+      h2s.forEach(h2 => {
+        h2.style.color = this.barbieTheme[2]
+      });
+
+      // toggle card colors
+      let cards = Array.from(document.getElementsByClassName('card'))
+      cards.forEach(card => {
+        card.classList.remove('ken')
+      });
+
+      // toggle tag colors
+      let tags = Array.from(document.getElementsByClassName('tag'))
+      tags.forEach(tag => {
+        let onetag = tag as HTMLElement
+        onetag.style.color = this.barbieTheme[1]
+        onetag.style.borderColor = this.barbieTheme[1]
+      })
+    
+      // toggle bg color
+      document.getElementsByTagName('main')[0].style.backgroundColor = this.barbieTheme[0]
       document.getElementsByTagName('body')[0].style.backgroundColor = this.barbieTheme[0]
-    }
+
+      // toggle h1, button
+      document.getElementsByTagName('h1')[0].style.color = this.barbieTheme[1]
+      document.getElementById('open-cv')?.classList.remove('ken')    }
   }
 
 }
