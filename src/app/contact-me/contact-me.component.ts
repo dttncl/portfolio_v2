fb0e53fefbcbeec2d98eaa2d7c0639833dd58c15
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-12 14:24:35 -0400

    added contact form

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EnvironmentInjector } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { EmailService } from '../email.service';
@@ -18,13 +18,41 @@ export class ContactMeComponent {
     name : new FormControl(''),
     email : new FormControl('',Validators.required),
     subject : new FormControl('Awesome Website',Validators.maxLength(20)),
-    message : new FormControl('Hey Bernadette! I love what you did here.',Validators.required)
+    message : new FormControl('Hey Bernadette! I love what you did here.',Validators.required),
+    access_key : new FormControl('7200e0e6-f4c8-42fa-9a8c-bda32f224266')
   });
 
   onSubmit() {
-    //console.warn(this.contactForm.value);
-    this.emailService.sendEmail(this.contactForm).subscribe(
+    /*
+    const data = {
+      access_key: '7200e0e6-f4c8-42fa-9a8c-bda32f224266',
+      name: this.contactForm.get('name')!.value!,
+      email: this.contactForm.get('email')!.value!,
+      subject: this.contactForm.get('subject')!.value!,
+      message: this.contactForm.get('message')!.value!,
+    };
+    */
+
+    let data = this.contactForm;
+    console.log(this.emailService.sendEmail(data));
+    
+  }
+
+  /*
+  onSubmit() {
+    const data = {
+      name: this.contactForm.get('name')!.value!,
+      email: this.contactForm.get('email')!.value!,
+      subject: this.contactForm.get('subject')!.value!,
+      message: this.contactForm.get('message')!.value!,
+      access_key: '7200e0e6-f4c8-42fa-9a8c-bda32f224266'
+    };
+  
+    console.log(data);
+
+    this.emailService.sendEmail(data).subscribe(
       response => {
+        console.log(response);
         console.log('Email sent successfully!');
       },
       error => {
@@ -32,5 +60,6 @@ export class ContactMeComponent {
       }
     );
   }
+  */
 
 }
--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
@@ -7,21 +7,56 @@ import { FormGroup } from '@angular/forms';
 })
 export class EmailService {
 
-  private emailUrl = 'http://localhost:3000/send-email'; // Replace with your backend URL
-
+  private emailUrl = 'https://api.web3forms.com/submit'; 
+/*
   constructor(private http: HttpClient) { }
 
-  sendEmail(contactForm : FormGroup) {
+  
+  sendEmail(data : any) {
+    
+    
     const data = {
       name : contactForm.get('name')?.value,
       email: contactForm.get('email')?.value,
       subject: contactForm.get('subject')?.value,
-      message: contactForm.get('message')?.value
+      message: contactForm.get('message')?.value,
+      access_key: "7200e0e6-f4c8-42fa-9a8c-bda32f224266"
     };
-
+    
+    
     console.log(data)
 
-    return this.http.post(this.emailUrl, data);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
+    });
+
+    return this.http.post(this.emailUrl, data, { headers: headers });
   }
+  */
+  
+  async sendEmail(data : FormGroup) {
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("access_key", "7200e0e6-f4c8-42fa-9a8c-bda32f224266");
+    urlencoded.append("name", "Bernadette");
+    urlencoded.append("email", "dette@363@gmail.com");
+    urlencoded.append("subject", "Hehe");
+    urlencoded.append("message", "Hehe"); 
+
+    const response = await fetch(this.emailUrl, {
+      method: 'POST',
+      body: urlencoded,
+      headers: myHeaders
+    });
+
+    return await response.json();
+  }
+  
+
 
 }
